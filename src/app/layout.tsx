76e669751fd826c52653d83503425bf555dcc89c
Júlio Mühlbauer <juliowernermm@gmail.com>
2025-02-22 21:18:38 -0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import {
-	assertIsLocale,
-	baseLocale,
-	getLocale,
-	Locale,
-	overwriteGetLocale,
+  assertIsLocale,
+  baseLocale,
+  getLocale,
+  Locale,
+  overwriteGetLocale,
 } from "../paraglide/runtime";
 import ClientSideLocaleSwitch from "./ClientSideLocaleSwitch";
 import { m } from "../paraglide/messages.js";
@@ -16,24 +16,25 @@ const ssrLocale = cache(() => ({ locale: baseLocale }));
 overwriteGetLocale(() => assertIsLocale(ssrLocale().locale));
 
 export default async function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	// @ts-expect-error - headers must be sync
-	// https://github.com/opral/inlang-paraglide-js/issues/245#issuecomment-2608727658
-	ssrLocale().locale = headers().get("x-paraglide-locale") as Locale;
+  // @ts-expect-error - headers must be sync
+  // https://github.com/opral/inlang-paraglide-js/issues/245#issuecomment-2608727658
+  ssrLocale().locale = headers().get("x-paraglide-locale") as Locale;
+  // ssrLocale().locale = (await headers()).get("x-paraglide-locale") as Locale;
 
-	return (
-		<html lang={getLocale()}>
-			<body>
-				<b>
-					<p>{m.programmatic_locale_switching_info()}</p>
-				</b>
-				<ClientSideLocaleSwitch />
-				<hr />
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang={getLocale()}>
+      <body>
+        <b>
+          <p>{m.programmatic_locale_switching_info()}</p>
+        </b>
+        <ClientSideLocaleSwitch />
+        <hr />
+        {children}
+      </body>
+    </html>
+  );
 }
