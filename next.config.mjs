c195e155a6a0d0ac33b41c5dd8812b3bf5b0d434
Júlio Mühlbauer <juliowernermm@gmail.com>
2025-02-25 20:42:47 -0300

    url strategy

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,15 @@ import { paraglideWebpackPlugin } from "@inlang/paraglide-js";
  * @type {import('next').NextConfig}
  */
 export default {
-	webpack: (config) => {
-		config.plugins.push(
-			paraglideWebpackPlugin({
-				outdir: "./src/paraglide",
-				project: "./project.inlang",
-			})
-		);
+  webpack: (config) => {
+    config.plugins.push(
+      paraglideWebpackPlugin({
+        outdir: "./src/paraglide",
+        project: "./project.inlang",
+        strategy: ["url"],
+      })
+    );
 
-		return config;
-	},
+    return config;
+  },
 };
